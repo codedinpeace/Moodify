3b64206807be2c508ccdb2d723825eca35120994
codedinpeace <vea5a.pranam05@gmail.com>
2026-04-04 10:46:36 +0530

    Added blacklisting feature

--- Backend/src/middlewares/verifyUser.js
+++ Backend/src/middlewares/verifyUser.js
@@ -1,9 +1,15 @@
 const jwt = require('jsonwebtoken')
+const blackListModel = require('../models/blackList.mode')
 
 const verifyUser = async (req,res,next) => {
     try {
         const token = req.cookies.token
         if(!token) return res.status(403).json({message:"Invalid Token"})
+
+            const blackListedToken = await blackListModel.findOne({
+                token
+            })
+            if(blackListedToken) return res.status(401).json({message:"unauthorized token"})
 
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
             req.user = decoded
