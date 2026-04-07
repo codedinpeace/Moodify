e5a50fcd51353ab55048507ad7856438ae05c80d
codedinpeace <vea5a.pranam05@gmail.com>
2026-04-07 10:01:15 +0530

    added functions performing authentication

---
+++ Frontend/src/features/auth/services/api.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL:"http://localhost:8000/api",
+    withCredentials:true
+})
+
+export const register = async (username, email, password) => {
+    try {
+        const response = await api.post("/auth/register", {username, email, password})
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const login =async  (username, email, password) => {
+    try {
+        const response = await api.post("/auth/login", {username, email, password})
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const check = async () => {
+    try {
+        const response = await api.get("/auth/check")
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const logout = async () => {
+    try {
+        await api.post("/auth/logout")
+    } catch (error) {
+        console.log(error)
+    }
+}
