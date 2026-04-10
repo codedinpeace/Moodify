1b9305460233e0eef0f3d1036622b2720c3feeeb
codedinpeace <vea5a.pranam05@gmail.com>
2026-04-10 12:47:23 +0530

    made the controllers

--- Backend/src/controllers/song.controllers.js
+++ Backend/src/controllers/song.controllers.js
@@ -1,5 +1,9 @@
+const mm = require("music-metadata");
+
 const createSongs = async (req,res) => {
-    
+    const {mood} = req.query    
+    const metadata = await mm.parseBuffer(req.file.buffer);
+    res.send(metadata, mood)
 }
 const getSongs = async (req,res) => {
 
--- Backend/src/routes/song.routes.js
+++ Backend/src/routes/song.routes.js
@@ -4,7 +4,7 @@ const verifyUser = require("../middlewares/verifyUser")
 const upload = require('../services/multer.service')
 const songControllers = require('../controllers/song.controllers')
 
-songRouter.post("/create-post",upload.single('song'), verifyUser, songControllers.getSongs)
-songRouter.post("/get-post", verifyUser, songControllers.createSongs)
+songRouter.post("/create-song",upload.single('song'), verifyUser, songControllers.createSongs)
+songRouter.post("/get-song", verifyUser, songControllers.getSongs   )
 
 module.exports = songRouter
